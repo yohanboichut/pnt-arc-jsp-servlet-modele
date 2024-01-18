4d9c1e98797d5b261ef2afb8a51a881e0c0c444c
Boichut Yohan <yohan.boichut@univ-orleans.fr>
2024-01-18 02:10:39 +0100

    Correction

--- src/main/java/modele/ResultatQCM.java
+++ src/main/java/modele/ResultatQCM.java
@@ -12,12 +12,19 @@ public class ResultatQCM {
     private Map<EtudiantDTO,Score> scores=new HashMap<>();
 
     public ResultatQCM(String cleQCM, String description, QuestionItem[] questions, Collection<Etudiant> participants) {
-        participants.stream().forEach(participant -> scores.put(new EtudiantDTO(participant.getEmail(), participant.getPseudo()), new Score(cleQCM, description, questions.length)));
+        Map<Etudiant,EtudiantDTO> mapping = new HashMap<>();
+        participants.stream().forEach(participant ->
+        {
+            EtudiantDTO dto = new EtudiantDTO(participant.getEmail(), participant.getPseudo());
+            mapping.put(participant,dto);
+            scores.put(dto, new Score(cleQCM, description, questions.length));
+        });
         for(QuestionItem question : questions){
             for (int i= 0;i<question.reponses.length;i++){
                 if (question.reponses[i].equals(question.bonneReponse)){
                     for (Utilisateur participant : question.reponsesUtilisateurs[i]){
-                        scores.get(participant).ajouterBonneReponse();
+
+                        scores.get(mapping.get(participant)).ajouterBonneReponse();
                     }
                 }
             }
